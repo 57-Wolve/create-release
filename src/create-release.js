6af2393e7ea663d0b0be3e5f28935ff9e7ff3838
William Gill <william.gill@anomalous.dev>
2024-04-09 23:39:52 -0500

    Update create-release.js

--- src/create-release.js
+++ src/create-release.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 async function run() {
   try {
     // Get authenticated GitHub client (Ocktokit): https://github.com/actions/toolkit/tree/master/packages/github#usage
-    const github = new GitHub(process.env.GITHUB_TOKEN);
+    const github = new GitHub(process.env.GITEA_TOKEN);
 
     // Get owner and repo from context of payload that triggered the action
     const { owner: currentOwner, repo: currentRepo } = context.repo;
